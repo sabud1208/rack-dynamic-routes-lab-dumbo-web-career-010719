1af2b8c9c9bf1836fdc390134919188ffc94c753
Stephanie Abud <sabud1208@gmail.com>
2019-01-28 19:22:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,12 @@
 class Application 
  
-  
+   @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
 def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new
   
   if req.path.match('/items')
-    add_item 
+    add_item = 
 
 end
 end

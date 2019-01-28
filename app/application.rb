8e70f1e0e67f018183eca9d8eb0e5029a10739c4
Stephanie Abud <sabud1208@gmail.com>
2019-01-28 19:27:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
    @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
 def call(env)
   resp = Rack::Response.new
-  req = Rack::Request.new
+  req = Rack::Request.new(env)
   
   if req.path.match('/items')
    item = req.path.split("/items/").last

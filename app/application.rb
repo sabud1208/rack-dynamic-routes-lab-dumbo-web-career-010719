b43586382026ebeff7c82fb3bb82b9b7a14584c0
Stephanie Abud <sabud1208@gmail.com>
2019-01-28 19:27:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,17 @@
-class Application 
  
-   @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
-def call(env)
-  resp = Rack::Response.new
-  req = Rack::Request.new
-  
-  if req.path.match('/items')
-   item = req.path.split("/items/").last
-   if items = @@items.find{ |x| x.name == item}
-        resp.write item.price 
-     else    
-    resp.status = 400
+class Application
+
+  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else 
+        resp.status = 400
         resp.write "Item not found"
       end
     else
